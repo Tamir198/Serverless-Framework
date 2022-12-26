767ec93ba4f7be5f604ecc741273066a89e0ad18
Tamir Abutbul <1tamir198@gmail.com>
2022-12-26 23:48:36 +0200

    Adding option to add item to dynamoDB

--- auction-service/src/handlers/createAuction.js
+++ auction-service/src/handlers/createAuction.js
@@ -1,11 +1,19 @@
+import { v4 as uuid } from "uuid";
+import AWS from "aws-sdk";
+
 async function createAuction(event, context) {
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
   const { title } = JSON.parse(event.body);
 
   const auction = {
     title: title,
+    id: uuid.call(),
     status: "open",
     creationTime: new Date().toISOString(),
   };
+
+  //The aws sdk is using callbacks, the promise in here so we can use await
+  await dynamoDb.put({ TableName: "AuctionsTable", Item: auction }).promise();
 
   return {
     statusCode: 201,
